848b53c6effd6fdd41ae2dcb2a214178239a0908
Nurman2022 <nurman.awaluddin2017@gmail.com>
2024-11-25 16:16:28 +0800

    update layout.ts

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,4 @@
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$lib/Env.js';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 import { createBrowserClient, isBrowser, parse } from "@supabase/ssr"
 import type { LayoutLoad } from "./$types"
 
