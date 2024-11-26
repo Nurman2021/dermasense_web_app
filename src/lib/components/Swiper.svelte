4fb5757e3448423396728a035270c5e7a5ec2f59
Nurman2021 <nurman.awaluddin@student.unismuh.ac.id>
2024-11-27 06:42:18 +0800

    update product content

--- src/lib/components/Swiper.svelte
+++ src/lib/components/Swiper.svelte
@@ -1,6 +1,6 @@
 <script>
 	// import Swiper core and required modules
-	import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
+	import { Navigation, Scrollbar, A11y, Autoplay } from 'swiper';
 	import { Divider, Button, Icon } from 'stdf';
 
 	import { Swiper, SwiperSlide } from 'swiper/svelte';
@@ -25,11 +25,11 @@
 			class="card-style mt-5 flex flex-col items-center rounded-xl bg-white dark:bg-gray-800 dark:text-white"
 		>
 			<div>
-				<image src="/images/product/mockup_2.png" alt="image content" style="width:400px" />
+				<image src="/images/product/soap_2.png" alt="image content" style="width:400px" />
 			</div>
 			<div class="flex w-full items-center justify-between gap-4 px-4">
 				<div class="space-y-4">
-					<h3 class="text-4xl font-bold">Aromaterapi</h3>
+					<h3 class="text-4xl font-bold">Sabun Aromaterapi</h3>
 					<p class="mb-5 text-sm font-light">
 						VALITIC Kojic Acid Dark Spot Remover Soap Bars with Vitamin C, Retinol, Collagen,
 						Turmeric
@@ -54,11 +54,11 @@
 			class="card-style mt-5 flex flex-col items-center rounded-xl bg-white dark:bg-gray-800 dark:text-white"
 		>
 			<div>
-				<image src="/images/product/mockup_1.png" alt="image content" style="width:400px" />
+				<image src="/images/product/soap_1.png" alt="image content" style="width:400px" />
 			</div>
 			<div class="flex w-full items-center justify-between gap-4 px-4">
 				<div class="space-y-4">
-					<h3 class="text-4xl font-bold">Eksfoliasi</h3>
+					<h3 class="text-4xl font-bold">Sabun Eksfoliasi</h3>
 					<p class="mb-5 text-sm font-light">
 						VALITIC Kojic Acid Dark Spot Remover Soap Bars with Vitamin C, Retinol, Collagen,
 						Turmeric
@@ -83,11 +83,100 @@
 			class="card-style mt-5 flex flex-col items-center rounded-xl bg-white dark:bg-gray-800 dark:text-white"
 		>
 			<div>
-				<image src="/images/product/mockup_3.png" alt="image content" style="width:400px" />
+				<image src="/images/product/soap_3.png" alt="image content" style="width:400px" />
 			</div>
 			<div class="flex w-full items-center justify-between gap-4 px-4">
 				<div class=" space-y-4">
-					<h3 class="text-4xl font-bold">Minyak Esensial</h3>
+					<h3 class="text-4xl font-bold">Sabun Minyak Esensial</h3>
+					<p class="mb-5 text-sm font-light">
+						VALITIC Kojic Acid Dark Spot Remover Soap Bars with Vitamin C, Retinol, Collagen,
+						Turmeric
+					</p>
+
+					<p>Share or Save for later</p>
+				</div>
+				<div>
+					<Icon name="ri-share-line" size={40} />
+				</div>
+			</div>
+			<div class="w-full">
+				<Divider px="4" />
+			</div>
+			<div class="mb-4 w-full font-bold">
+				<Button radius="xl">PESAN</Button>
+			</div>
+		</div>
+	</SwiperSlide>
+
+	<!-- lotion section -->
+	<SwiperSlide>
+		<div
+			class="card-style mt-5 flex flex-col items-center rounded-xl bg-white dark:bg-gray-800 dark:text-white"
+		>
+			<div>
+				<image src="/images/product/lotion_2.png" alt="image content" style="width:228px" />
+			</div>
+			<div class="flex w-full items-center justify-between gap-4 px-4">
+				<div class="space-y-4">
+					<h3 class="text-4xl font-bold">Lotion Aromaterapi</h3>
+					<p class="mb-5 text-sm font-light">
+						VALITIC Kojic Acid Dark Spot Remover Soap Bars with Vitamin C, Retinol, Collagen,
+						Turmeric
+					</p>
+
+					<p>Share or Save for later</p>
+				</div>
+				<div>
+					<Icon name="ri-share-line" size={40} />
+				</div>
+			</div>
+			<div class="w-full">
+				<Divider px="4" />
+			</div>
+			<div class="mb-4 w-full font-bold">
+				<Button radius="xl">PESAN</Button>
+			</div>
+		</div>
+	</SwiperSlide>
+	<SwiperSlide>
+		<div
+			class="card-style mt-5 flex flex-col items-center rounded-xl bg-white dark:bg-gray-800 dark:text-white"
+		>
+			<div>
+				<image src="/images/product/lotion_1.png" alt="image content" style="width:228px" />
+			</div>
+			<div class="flex w-full items-center justify-between gap-4 px-4">
+				<div class="space-y-4">
+					<h3 class="text-4xl font-bold">Lotion Eksfoliasi</h3>
+					<p class="mb-5 text-sm font-light">
+						VALITIC Kojic Acid Dark Spot Remover Soap Bars with Vitamin C, Retinol, Collagen,
+						Turmeric
+					</p>
+
+					<p>Share or Save for later</p>
+				</div>
+				<div>
+					<Icon name="ri-share-line" size={40} />
+				</div>
+			</div>
+			<div class="w-full">
+				<Divider px="4" />
+			</div>
+			<div class="mb-4 w-full font-bold">
+				<Button radius="xl">PESAN</Button>
+			</div>
+		</div>
+	</SwiperSlide>
+	<SwiperSlide>
+		<div
+			class="card-style mt-5 flex flex-col items-center rounded-xl bg-white dark:bg-gray-800 dark:text-white"
+		>
+			<div>
+				<image src="/images/product/lotion_3.png" alt="image content" style="width:228px" />
+			</div>
+			<div class="flex w-full items-center justify-between gap-4 px-4">
+				<div class=" space-y-4">
+					<h3 class="text-4xl font-bold">Lotion Minyak Esensial</h3>
 					<p class="mb-5 text-sm font-light">
 						VALITIC Kojic Acid Dark Spot Remover Soap Bars with Vitamin C, Retinol, Collagen,
 						Turmeric
