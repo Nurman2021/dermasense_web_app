66bc888b24e15fe67ca3a668cf63a0f0362dd6ef
Nurman2021 <nurman.awaluddin@student.unismuh.ac.id>
2024-12-01 04:13:21 +0800

    add event

--- src/routes/(pages)/faceScanner/+page.svelte
+++ src/routes/(pages)/faceScanner/+page.svelte
@@ -1,11 +1,42 @@
-<iframe
-	src="https://beekman1802.com/pages/skin-biome-analysis"
-	class="mx-auto h-screen w-full max-w-[750px] rounded-lg border-2 border-gray-300"
-	allowfullscreen
-	allow="camera; microphone"
-	id="myFrame"
->
-</iframe>
-<!-- url#1 =>https://cloud.email.cetaphil.com/skin-selfie.html -->
-<!-- url#2 =>https://skin-analyzer.vlcc.com/analysis -->
-<h1>Data dari iframe:</h1>
+<script>
+	import { onMount, onDestroy } from 'svelte';
+
+	let overlayGone = false;
+
+	function hideOverlay() {
+		overlayGone = true;
+		alert('SOME CLICK');
+	}
+
+	onMount(() => {
+		// let theButton = document.getElementById('joe');
+		document.addEventListener('click', hideOverlay);
+	});
+
+	onDestroy(() => {
+		document.removeEventListener('click', hideOverlay);
+	});
+</script>
+
+<section class="h-screen overflow-hidden bg-[#F3F3F4]">
+	<iframe
+		onloadeddata={() => alert('meta data berubah')}
+		title="beekman scanner skin"
+		src="https://beekman1802.com/pages/skin-biome-analysis"
+		class="relative -top-[27rem] mx-auto min-h-[200vh] max-w-[350px]"
+		allowfullscreen
+		allow="camera; microphone"
+		id="myFrame"
+		scrolling="no"
+	>
+	</iframe>
+	<div
+		class={overlayGone
+			? 'hidden'
+			: ' pointer-events-none fixed top-0 flex h-full w-full items-center justify-center bg-darkBlack opacity-50'}
+	>
+		<!-- <button class="p-10" id="joe">CLIDK ME</button> -->
+	</div>
+	<!-- url#1 =>https://cloud.email.cetaphil.com/skin-selfie.html -->
+	<!-- url#2 =>https://skin-analyzer.vlcc.com/analysis -->
+</section>
